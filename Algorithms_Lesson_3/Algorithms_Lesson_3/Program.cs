93b004961d1d122750110b12808baeca66728672
bormannVadim <freedom-mv47@rambler.ru>
2019-07-11 21:45:18 +0300

    Сделал 3 задание на C# =)

---
+++ Algorithms_Lesson_3/Algorithms_Lesson_3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Algorithms_Lesson_3
+{
+
+    // Савенко В.
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //Линейный поиск
+            int[] array = FillArray(10);
+            ShowArray(array);
+
+            string ElementFound = SearchEl(array, 2, 0) != -1 ? "Елемент найден!" :"Елемент не найден!";
+            Console.WriteLine(ElementFound);
+        }
+
+
+        // Линейный поиск в виде рекурсии
+        public static int[] FillArray(int size)
+        {
+            Random r1 = new Random();
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = r1.Next(0, 10);
+            }
+            return array;
+        }
+
+        public static void ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ": " + array[i]);
+            }
+        }
+
+        public static int SearchEl(int[] array,int el,int index)
+        {
+            if (array.Length != index)
+            {
+                if (array[index]==el)
+                 {
+                          return index;
+                 }
+                else
+                {
+                
+                    index++;
+                    return SearchEl(array, el, index++);
+               
+                 }
+            }
+            else
+                return -1;
+        }
+    }
+}
